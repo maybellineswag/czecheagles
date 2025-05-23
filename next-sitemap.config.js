2c4cff367a88a7fbff658ec6496378a49f6c73f1
maybellineswag <advantagesofkindness@gmail.com>
2025-05-23 20:31:42 +0200

    Quick update

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://www.czecheaglesmma.cz', // TODO: Replace with your real domain
+  generateRobotsTxt: true, // Generate robots.txt
+  // You can add more options here if needed
+} 
